1e2b8ca9a8429a9dd2ddee0c2a99407566ff5333
上海海洋大学 <shanhaikaiyoudaigaku@shanghaihaiyangdaxuedeimac-2.local>
2017-03-29 20:45:18 +0800

    第三周

---
+++ Calculate/Calculate/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Calculate
+//
+//  Created by 上海海洋大学 on 17/3/29.
+//  Copyright © 2017年 上海海洋大学. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping{
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
+    }
+    
+    var displayValue:Double{
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle{
+            switch mathematicalSymbol{
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+    
+    
+    
+    
+    func drawHorizontaline(form startX:Double,to endX:Double,using color: UIColor){
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
